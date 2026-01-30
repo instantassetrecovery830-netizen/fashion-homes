c40a007ee0b6e55cdab4025f6c3c4152581802b8
instantassetrecovery830-netizen <instantassetrecovery830@gmail.com>
2026-01-30 06:03:08 -0500

    refactor: Remove unused Firebase storageBucket config
    The `storageBucket` configuration was not being used and has been removed to simplify the Firebase configuration object.

--- services/firebase.ts
+++ services/firebase.ts
@@ -21,7 +21,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyBBeM4QB0gkxenQHyGh3vyBz91_-q4qL0o",
   authDomain: "myfitstore-922b4.firebaseapp.com",
   projectId: "myfitstore-922b4",
-  storageBucket: "myfitstore-922b4.firebasestorage.app",
   messagingSenderId: "85430306162",
   appId: "1:85430306162:web:0190d789d2a032f15249eb"
 };
