af3e172011e5f1db6b928bf1bd4c43a47114db57
instantassetrecovery830-netizen <instantassetrecovery830@gmail.com>
2026-01-30 21:03:32 -0500

    refactor: Remove unused Firebase imports
    Removed commented-out code and unused imports related to Firebase initialization in the services/firebase.ts file. This cleans up the code and removes unnecessary dependencies.

--- services/firebase.ts
+++ services/firebase.ts
@@ -12,11 +12,6 @@ export interface User {
   reload: () => Promise<void>;
 }
 
-// Import the functions you need from the SDKs you need  
-import { initializeApp } from "firebase/app";  
-// TODO: Add SDKs for Firebase products that you want to use  
-// https://firebase.google.com/docs/web/setup#available-libraries  
-  
 // Your web app's Firebase configuration  
 const firebaseConfig = {  
   apiKey: "AIzaSyBZkNjNGQT0A7sKnWj2dNCJxyvS8d5OxOA",  
